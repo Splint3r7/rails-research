b5dedab245ae578d4f90230b9761c271e3f276c8
hassan.khan@sendoso.com <hassankhan14044@outlook.com>
2021-04-09 16:54:38 +0500

    Update Dconfison.py

--- rubygem_404/Dconfusion.py
+++ rubygem_404/Dconfusion.py
@@ -8,6 +8,7 @@ import argparse
 import sys,os
 import concurrent.futures
 import re
+from bs4 import BeautifulSoup
 
 if os.name == 'nt':
 	os.system('cls')
@@ -22,17 +23,22 @@ def logo():
 	| |_| |  __| | | | | |___) | (_| (_| | | | | | | |  __| |
  	\____|\___|_| |_| |_|____/ \___\__,_|_| |_|_| |_|\___|_|
 
- 	GemScanner - Finds depreciated versions of your gems.
+ 	GemScanner - Finds Vulnerable/Claimable RubyGems!.
  	Author: Splint3r7 - ( https://github.com/Splint3r7 )
 
 		""")
 
-parser = argparse.ArgumentParser(description="Script to find vulnerable GEMSs")
+parser = argparse.ArgumentParser(description="Script to find vulnerable GEMS")
 
 parser.add_argument('-f','--file',
                             help = "Path to Gemfile.lock",
                             type = str,
-                            required = True)
+                            required = False)
+
+parser.add_argument('-u','--url',
+                            help = "URL to Gemfile.lock",
+                            type = str,
+                            required = False)
 
 parser.add_argument('-o','--output',
                             help = "Output file",
@@ -41,8 +47,6 @@ parser.add_argument('-o','--output',
 
 args = parser.parse_args()
 
-file_path = args.file
-out_file = args.output
 
 def versions_f(_gemname_):
 
@@ -58,38 +62,72 @@ def versions_f(_gemname_):
 	except:
 		pass
 
+def versions_url(_gemname_):
+
+	print(Style.DIM+Fore.GREEN+"[-]"+Style.RESET_ALL+Style.DIM+Fore.WHITE+" TESTING | "+Style.RESET_ALL+Style.BRIGHT+Fore.BLUE+"{}".format(_gemname_)+Style.RESET_ALL)
+	try:
+		req = requests.get("https://rubygems.org/gems/"+_gemname_, timeout=7, allow_redirects=False)
+		if req.status_code == 404:
+			print(Style.DIM+Fore.YELLOW+"["+Style.BRIGHT+Fore.GREEN+"+"+Style.RESET_ALL+Style.DIM+Fore.YELLOW+"]"+Style.RESET_ALL+Style.BRIGHT+Fore.RED+" CLAIMABLE GEM FOUND |"+Style.BRIGHT+Fore.RED+" {}".format(_gemname_)+Style.RESET_ALL)
+			filew.write(_gemname_+"\n")
+		else:
+			pass
+	except:
+		pass
+
+def GemParser(_array_):
+
+	for line in _array_:
+		line = line.strip()
+		if "(" in line:
+			fullname = line
+			line_arr = line.split(" ", 1)
+			# To get the gem name from gem (version) basically from lock files
+			gem_name = line_arr[0]
+			gem_version = line_arr[1]
+			_lista_.append(gem_name)
+
+				# to read gem 'gem_name'	
+		elif ":" not in line and not "source" in line and not "GEM" in line and "gem" in line:
+			line = line.split("'")
+			linex = line[0:2]
+			for y in linex:
+				y = y.strip()
+				if not "gem" in y:
+					_lista_.append(y)
+
+				# to read rest of the dependencies
+		elif "https://rubygems.org" not in line and "#" not in line and "GEM" not in line and ":" not in line and "PLATFORMS" not in line and "DEPENDENCIES" not in line and "1.16.0" not in line and "BUNDLED WITH" not in line:
+					_lista_.append(line)
+
 
 if __name__ == "__main__":
 
-	logo()
+	out_file = args.output
 	_lista_ = []
 	unique_list = []
-
 	filew = open(out_file, "a+")
-	with open(file_path, "r") as f:
-		lines = f.readlines()
-		for line in lines:
-			line = line.strip()
-			if "(" in line:
-				fullname = line
-				line_arr = line.split(" ", 1)
-				# To get the gem name from gem (version) basically from lock files
-				gem_name = line_arr[0]
-				gem_version = line_arr[1]
-				_lista_.append(gem_name)
 
-			# to read gem 'gem_name'	
-			elif ":" not in line and not "source" in line and not "GEM" in line and "gem" in line:
-				line = line.split("'")
-				linex = line[0:2]
-				for y in linex:
-					y = y.strip()
-					if not "gem" in y:
-						_lista_.append(y)
+	if args.file:
+		file_path = args.file
+		logo()
+		with open(file_path, "r") as f:
+			lines = f.readlines()
+			GemParser(lines)
 
-			# to read rest of the dependencies
-			elif "https://rubygems.org" not in line and "#" not in line and "GEM" not in line and ":" not in line:
-				_lista_.append(line)			
+
+	elif args.url:
+
+		logo()
+		f2 = open("temp.txt", "w")
+		url = args.url
+		req = requests.get(url, timeout=10)
+		soup = BeautifulSoup(req.content, 'html.parser')
+		f2.write(str(soup))
+		f2.close()
+		with open("temp.txt", "r") as f:
+			lines = f.readlines()
+			GemParser(lines)
 
 	for x in _lista_:
 		if x not in unique_list:
